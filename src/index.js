3c48da9b5e2a22cbb89f36cef31a4054544ccf9e
Tatiana Volkova <mtatiana2402@gmail.com>
2020-10-12 21:18:19 +0200

    Add solution to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = String(n);
+  var newN = n.split('').reverse().filter(function(el){
+      return el !=='-';
+  });
+  newN = newN.join('');
+  return Number(newN);
 }
